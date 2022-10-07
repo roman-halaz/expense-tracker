af9c849b5ac7c28e3e0056153e31cbc1014330fb
Roman <roman.halaz96@gmail.com>
2022-10-07 14:58:46 +0300

    Added useMemo

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './Chart.css';
 import ChartBar from './ChartBar';
 
 const Chart = (props) => {
   const { dataPoints } = props;
-  const dataPointValues = dataPoints.map((dataPoint) => dataPoint.value);
-  const totalMaximum = Math.max(...dataPointValues);
 
+  const dataPointValues = useMemo(
+    () => dataPoints.map((dataPoint) => dataPoint.value),
+    [],
+  );
+  const totalMaximum = useMemo(() => Math.max(...dataPointValues), []);
+  // const dataPointValues = dataPoints.map((dataPoint) => dataPoint.value);
+  // const totalMaximum = Math.max(...dataPointValues);
+
+  // console.log(dataPointValues, 'dataPointValues');
+  // console.log(totalMaximum);
   return (
     <div className="chart">
       {dataPoints.map((dataPoint) => (
